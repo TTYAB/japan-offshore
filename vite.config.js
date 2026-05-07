823fcbb3e9a3a9d7e1b24b451546e252f1670272
TTYAB <137489329+TTYAB@users.noreply.github.com>
2026-05-07 10:34:45 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  // dataフォルダを直接公開ディレクトリとして使う
+  publicDir: 'public',
+  build: {
+    outDir: 'dist',
+  },
 });
